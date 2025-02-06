c51467e4c0ec61804f85bcb53d9ccd8dbd8bbdfe
Nivas Pasupuleti <nivasnani000@gmail.com>
2025-02-06 09:57:02 +0530

    New

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add interactivity to the "Predict Sentiment" button
+document.getElementById('predictBtn').addEventListener('click', () => {
+    alert('Sentiment prediction feature coming soon!');
+  });
